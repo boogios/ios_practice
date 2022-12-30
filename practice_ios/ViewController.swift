7dd8cdf78a0d4f8ccb609b8cd3cbb86590bf6bb8
tkddn0518 <tkddn0518@naver.com>
2022-12-30 19:59:54 +0900

    Initial Commit

---
+++ practice_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice_ios
+//
+//  Created by 변상우 on 2022/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
